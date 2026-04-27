d3c8bc9842b976a83a011275c5ce28df7c5af743
Taha Murtaza Ali <tahamurtaza21@outlook.com>
2026-04-27 12:58:02 +0500

    Added a different space to avoid downloading vulnerabilities

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -2,13 +2,11 @@
 
 echo "Scanning base image..."
 
-docker run --rm -v "$(pwd)":/root/.cache/ \
-  -e TRIVY_TIMEOUT=10m \
+docker run --rm -v /tmp/trivy-cache:/root/.cache/ \
   -e TRIVY_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-db \
   aquasec/trivy:latest image --exit-code 0 --severity HIGH eclipse-temurin:8-jdk-alpine
 
-docker run --rm -v "$(pwd)":/root/.cache/ \
-  -e TRIVY_TIMEOUT=10m \
+docker run --rm -v /tmp/trivy-cache:/root/.cache/ \
   -e TRIVY_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-db \
   aquasec/trivy:latest image --exit-code 1 --severity CRITICAL eclipse-temurin:8-jdk-alpine
 
