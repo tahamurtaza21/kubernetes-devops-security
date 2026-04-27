fa19a7e4df91e955a832cc2b73e15e4d40d30cfb
Taha Murtaza Ali <tahamurtaza21@outlook.com>
2026-04-27 13:37:20 +0500

    used a vulnerable image to test

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -5,12 +5,12 @@ echo "Scanning base image..."
 docker run --rm -v /var/lib/trivy-cache:/root/.cache/ \
   -e TRIVY_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-db \
   -e TRIVY_JAVA_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-java-db \
-  aquasec/trivy:latest image --exit-code 0 --severity HIGH eclipse-temurin:8-jdk-alpine
+  aquasec/trivy:latest image --exit-code 0 --severity HIGH eclipse-temurin:8-jdk
 
 docker run --rm -v /var/lib/trivy-cache:/root/.cache/ \
   -e TRIVY_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-db \
   -e TRIVY_JAVA_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-java-db \
-  aquasec/trivy:latest image --exit-code 1 --severity CRITICAL eclipse-temurin:8-jdk-alpine
+  aquasec/trivy:latest image --exit-code 1 --severity CRITICAL eclipse-temurin:8-jdk
 
 exit_code=$?
 echo "Exit Code : $exit_code"
