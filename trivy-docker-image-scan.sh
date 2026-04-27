cbb272bb6e33b82c47f1aef3d4cea61b6f3d4946
Taha Murtaza Ali <tahamurtaza21@outlook.com>
2026-04-27 13:47:56 +0500

    Added another docker command

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -5,6 +5,11 @@ echo "Scanning base image..."
 docker run --rm -v /var/lib/trivy-cache:/root/.cache/ \
   -e TRIVY_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-db \
   -e TRIVY_JAVA_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-java-db \
+  aquasec/trivy:latest image eclipse-temurin:8-jdk
+
+docker run --rm -v /var/lib/trivy-cache:/root/.cache/ \
+  -e TRIVY_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-db \
+  -e TRIVY_JAVA_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-java-db \
   aquasec/trivy:latest image --exit-code 0 --severity HIGH eclipse-temurin:8-jdk
 
 docker run --rm -v /var/lib/trivy-cache:/root/.cache/ \
@@ -12,10 +17,10 @@ docker run --rm -v /var/lib/trivy-cache:/root/.cache/ \
   -e TRIVY_JAVA_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-java-db \
   aquasec/trivy:latest image --exit-code 1 --severity CRITICAL eclipse-temurin:8-jdk
 
-docker run --rm -v /var/lib/trivy-cache:/root/.cache/ \
-  -e TRIVY_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-db \
-  -e TRIVY_JAVA_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-java-db \
-  aquasec/trivy:latest image --severity CRITICAL python:3.4-alpine
+#docker run --rm -v /var/lib/trivy-cache:/root/.cache/ \
+#  -e TRIVY_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-db \
+#  -e TRIVY_JAVA_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-java-db \
+#  aquasec/trivy:latest image --severity CRITICAL python:3.4-alpine
 
 exit_code=$?
 echo "Exit Code : $exit_code"
