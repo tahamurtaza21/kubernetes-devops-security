d0accacfbd969933017ea125edc4fbbc83998cde
Taha Murtaza Ali <tahamurtaza21@outlook.com>
2026-05-04 11:54:38 +0500

    Skipping db update to reduce the time it takes to run this pipeline

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -5,17 +5,17 @@ echo "Scanning base image..."
 docker run --rm -v /var/lib/trivy-cache:/root/.cache/ \
   -e TRIVY_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-db \
   -e TRIVY_JAVA_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-java-db \
-  aquasec/trivy:latest image eclipse-temurin:8-jdk
+  aquasec/trivy:latest image --skip-db-update eclipse-temurin:8-jdk
 
 docker run --rm -v /var/lib/trivy-cache:/root/.cache/ \
   -e TRIVY_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-db \
   -e TRIVY_JAVA_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-java-db \
-  aquasec/trivy:latest image --exit-code 0 --severity HIGH eclipse-temurin:8-jdk
+  aquasec/trivy:latest image --skip-db-update --exit-code 0 --severity HIGH eclipse-temurin:8-jdk
 
 docker run --rm -v /var/lib/trivy-cache:/root/.cache/ \
   -e TRIVY_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-db \
   -e TRIVY_JAVA_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-java-db \
-  aquasec/trivy:latest image --exit-code 1 --severity CRITICAL eclipse-temurin:8-jdk
+  aquasec/trivy:latest image --skip-db-update --exit-code 1 --severity CRITICAL eclipse-temurin:8-jdk
 
 #docker run --rm -v /var/lib/trivy-cache:/root/.cache/ \
 #  -e TRIVY_DB_REPOSITORY=ghcr.io/aquasecurity/trivy-db \
